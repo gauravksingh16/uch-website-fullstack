a5cd2a4d2ad7f2902339643ba0edfd82a0f64430
gauravksingh16 <gaurav.12007335@lpu.in>
2024-07-04 18:28:27 +0530

    Admin Panel Update 4

--- uch-frontend/src/assets/Components/Dashboard/Dashboard.jsx
+++ uch-frontend/src/assets/Components/Dashboard/Dashboard.jsx
@@ -1,7 +1,9 @@
 import styles from "./Dashboard.module.css";
 import { IoIosNotifications } from "react-icons/io";
-import { FaCaretUp,FaCaretDown } from "react-icons/fa";
-
+import { FaCaretUp, FaCaretDown } from "react-icons/fa";
+import Search from "../Search/Search";
+import { GoPlus } from "react-icons/go";
+import { MdOutlineFileDownload } from "react-icons/md";
 
 const Dashboard = () => {
   return (
@@ -9,7 +11,7 @@ const Dashboard = () => {
       <div className={styles.Notch}>
         <div className={styles.heading}>Dashboard</div>
         <div className={styles.actionGrp}>
-            <IoIosNotifications className={styles.icon} />
+          <IoIosNotifications className={styles.icon} />
         </div>
       </div>
       <div className={styles.cardContent}>
@@ -17,7 +19,9 @@ const Dashboard = () => {
           <div className={styles.cardTitle}>Total Orders</div>
           <div className={styles.cardValue}>100</div>
           <div className={styles.compareLastMonth}>
-            <div className={`${styles.head} ${styles.positive}`}><FaCaretUp /> 8%</div>
+            <div className={`${styles.head} ${styles.positive}`}>
+              <FaCaretUp /> 8%
+            </div>
             <div className={styles.caps}>vs Last Month</div>
           </div>
         </div>
@@ -25,7 +29,9 @@ const Dashboard = () => {
           <div className={styles.cardTitle}>Total Revenue</div>
           <div className={styles.cardValue}>₹1000</div>
           <div className={styles.compareLastMonth}>
-            <div className={`${styles.head} ${styles.negative}`}><FaCaretDown /> 3%</div>
+            <div className={`${styles.head} ${styles.negative}`}>
+              <FaCaretDown /> 3%
+            </div>
             <div className={styles.caps}>vs Last Month</div>
           </div>
         </div>
@@ -33,7 +39,9 @@ const Dashboard = () => {
           <div className={styles.cardTitle}>Total Users</div>
           <div className={styles.cardValue}>1000</div>
           <div className={styles.compareLastMonth}>
-            <div className={`${styles.head} ${styles.positive}`}><FaCaretUp /> 3%</div>
+            <div className={`${styles.head} ${styles.positive}`}>
+              <FaCaretUp /> 3%
+            </div>
             <div className={styles.caps}>vs Last Month</div>
           </div>
         </div>
@@ -41,9 +49,99 @@ const Dashboard = () => {
           <div className={styles.cardTitle}>Product Listed</div>
           <div className={styles.cardValue}>100</div>
           <div className={styles.compareLastMonth}>
-            <div className={`${styles.head} ${styles.positive}`}><FaCaretUp /> 11%</div>
+            <div className={`${styles.head} ${styles.positive}`}>
+              <FaCaretUp /> 11%
+            </div>
             <div className={styles.caps}>vs Last Month</div>
           </div>
+        </div>
+      </div>
+      <div className={styles.tables}>
+        <div className={styles.userTable}>
+          <div className={styles.secNotch}>
+            <div className={styles.heading}>Users</div>
+            <div className={styles.actionGrp}>
+              <Search />
+              <MdOutlineFileDownload className={styles.icon} />
+            </div>
+          </div>
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone Number</th>
+                <th>Joined On</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>U1</td>
+                <td>John Doe</td>
+                <td>jhondoe@xyz.com</td>
+                <td>7970755957</td>
+                <td>12/12/2021</td>
+              </tr>
+              <tr>
+                <td>U1</td>
+                <td>John Doe</td>
+                <td>jhondoe@xyz.com</td>
+                <td>7970755957</td>
+                <td>12/12/2021</td>
+              </tr>
+              <tr>
+                <td>U1</td>
+                <td>John Doe</td>
+                <td>jhondoe@xyz.com</td>
+                <td>7970755957</td>
+                <td>12/12/2021</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className={styles.orderTable}>
+          <div className={styles.secNotch}>
+            <div className={styles.heading}>Orders</div>
+            <div className={styles.actionGrp}>
+              <Search />
+              <GoPlus className={styles.icon}/>
+            </div>
+          </div>
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone Number</th>
+                <th>Joined On</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>U1</td>
+                <td>John Doe</td>
+                <td>jhondoe@xyz.com</td>
+                <td>7970755957</td>
+                <td>12/12/2021</td>
+              </tr>
+              <tr>
+                <td>U1</td>
+                <td>John Doe</td>
+                <td>jhondoe@xyz.com</td>
+                <td>7970755957</td>
+                <td>12/12/2021</td>
+              </tr>
+              <tr>
+                <td>U1</td>
+                <td>John Doe</td>
+                <td>jhondoe@xyz.com</td>
+                <td>7970755957</td>
+                <td>12/12/2021</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
