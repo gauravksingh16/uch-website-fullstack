c85d50a640ab739bed88138213b236c37082f61f
gauravksingh16 <gaurav.12007335@lpu.in>
2024-07-06 09:18:22 +0530

    User timestamp update

--- uch-backend/models/User.js
+++ uch-backend/models/User.js
@@ -8,6 +8,7 @@ const userSchema = new mongoose.Schema({
   phone: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now},
 });
 
 userSchema.methods.generateAuthToken = function () {
